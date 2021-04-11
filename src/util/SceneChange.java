558b6267e2dfec725a939261a5700b8b01b5adec
nikhuff <nik.huff@me.com>
2021-04-11 14:53:04 -0700

    allow for scene changes with prebuilt controller

--- src/util/SceneChange.java
+++ src/util/SceneChange.java
@@ -1,31 +1,45 @@
 package util;
 
+import controller.Controller;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class SceneChange {
 
     private String viewUrl;
     private Stage stage;
+    private Controller controller;
 
     public SceneChange(Stage stage, String url) {
         this.stage = stage;
         this.viewUrl = url;
     }
 
+    public SceneChange(Stage stage, Controller controller) {
+        this.stage = stage;
+        this.controller = controller;
+    }
+
     public void changeScene(String title, int height, int width) {
         Parent root = null;
         try {
-            root = new FXMLLoader(getClass().getResource(viewUrl)).load();
+            if (controller != null) {
+                String url = controller.getViewURL();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource(url));
+                loader.setController(controller);
+                root = loader.load();
+            } else {
+                root = new FXMLLoader(getClass().getResource(viewUrl)).load();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Scene scene = new Scene(root, height, width);
+        Scene scene = new Scene(root, width, height);
         scene.getStylesheets().add("styles/styles.css");
         stage.setTitle(title);
         stage.setScene(scene);
