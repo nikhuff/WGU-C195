15d488cbad4eec1aaa12601c34647e43627d5f2a
nikhuff <nik.huff@me.com>
2021-04-10 21:50:06 -0700

    implement languages

--- src/controller/AppointmentDetail.java
+++ src/controller/AppointmentDetail.java
@@ -1,13 +1,57 @@
 package controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import util.Language;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AppointmentDetail implements Initializable {
+
+    @FXML
+    private Label appointmentTitle;
+    @FXML
+    private Label idLabel;
+    @FXML
+    private Label titleLabel;
+    @FXML
+    private Label locationLabel;
+    @FXML
+    private Label contactLabel;
+    @FXML
+    private Label typeLabel;
+    @FXML
+    private Label startLabel;
+    @FXML
+    private Label endLabel;
+    @FXML
+    private Label customerLabel;
+    @FXML
+    private Label userLabel;
+    @FXML
+    private Button delete;
+    @FXML
+    private Button cancel;
+    @FXML
+    private Button save;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        appointmentTitle.setText(Language.getField("Appointment"));
+        idLabel.setText(Language.getField("ID"));
+        titleLabel.setText(Language.getField("Title"));
+        locationLabel.setText(Language.getField("Location"));
+        contactLabel.setText(Language.getField("Contact"));
+        typeLabel.setText(Language.getField("Type"));
+        startLabel.setText(Language.getField("Start Time"));
+        endLabel.setText(Language.getField("End Time"));
+        customerLabel.setText(Language.getField("Customer ID"));
+        userLabel.setText(Language.getField("User ID"));
+        delete.setText(Language.getField("Delete"));
+        cancel.setText(Language.getField("Cancel"));
+        save.setText(Language.getField("Save"));
     }
 }
--- src/controller/CustomerDetail.java
+++ src/controller/CustomerDetail.java
@@ -1,14 +1,51 @@
 package controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import util.Language;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CustomerDetail implements Initializable {
 
+    @FXML
+    private Label customerTitle;
+    @FXML
+    private Label idLabel;
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private Label addressLabel;
+    @FXML
+    private Label zipcodeLabel;
+    @FXML
+    private Label divisionLabel;
+    @FXML
+    private Label countryLabel;
+    @FXML
+    private Label phoneLabel;
+    @FXML
+    private Button delete;
+    @FXML
+    private Button cancel;
+    @FXML
+    private Button save;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        customerTitle.setText(Language.getField("Customer"));
+        idLabel.setText(Language.getField("ID"));
+        nameLabel.setText(Language.getField("Name"));
+        addressLabel.setText(Language.getField("Address"));
+        zipcodeLabel.setText(Language.getField("Zipcode"));
+        divisionLabel.setText(Language.getField("Division"));
+        countryLabel.setText(Language.getField("Country"));
+        phoneLabel.setText(Language.getField("Phone"));
+        delete.setText(Language.getField("Delete"));
+        cancel.setText(Language.getField("Cancel"));
+        save.setText(Language.getField("Save"));
     }
 }
