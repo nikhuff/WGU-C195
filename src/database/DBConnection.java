360ef18bc0cd37d3635a30aed6e9ec041530980a
nikhuff <nik.huff@me.com>
2021-04-10 21:48:30 -0700

    implement db connectivity

---
+++ src/database/DBConnection.java
@@ -0,0 +1,44 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String protocol = "jdbc";
+    private static final String vendorName = ":mysql:";
+    private static final String ipAddress = "//wgudb.ucertify.com:3306/";
+    private static final String dbName = "WJ08q0i";
+
+    private static final String jdbcURL = protocol + vendorName + ipAddress + dbName;
+
+    private static final String MYSQLJDBCDriver = "com.mysql.jdbc.Driver";
+    private static Connection connection = null;
+
+    private static final String username = "U08q0i";
+    private static final String password = "53689364507";
+
+    public static Connection startConnection() {
+        try {
+            Class.forName(MYSQLJDBCDriver);
+            connection = DriverManager.getConnection(jdbcURL, username, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            connection.close();
+        } catch (Exception e) {
+
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+
+}
