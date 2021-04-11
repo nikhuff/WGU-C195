da47f36d73af5623b62ab8fd1bb25bf58455240c
nikhuff <nik.huff@me.com>
2021-04-11 14:50:19 -0700

    implement textfields

--- src/controller/AppointmentDetail.java
+++ src/controller/AppointmentDetail.java
@@ -1,15 +1,26 @@
 package controller;
 
+import database.DBContact;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import model.Appointment;
 import util.Language;
+import util.SceneChange;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class AppointmentDetail implements Initializable {
+public class AppointmentDetail implements Initializable, Controller {
+
+    private String viewURL;
+    private Appointment appointment;
 
     @FXML
     private Label appointmentTitle;
@@ -17,6 +28,8 @@ public class AppointmentDetail implements Initializable {
     private Label idLabel;
     @FXML
     private Label titleLabel;
+    @FXML
+    private Label descriptionLabel;
     @FXML
     private Label locationLabel;
     @FXML
@@ -32,18 +45,60 @@ public class AppointmentDetail implements Initializable {
     @FXML
     private Label userLabel;
     @FXML
+    private TextField idText;
+    @FXML
+    private TextField titleText;
+    @FXML
+    private TextField descriptionText;
+    @FXML
+    private TextField locationText;
+    @FXML
+    private ChoiceBox<String> contact;
+    @FXML
+    private TextField typeText;
+    @FXML
+    private TextField startText;
+    @FXML
+    private TextField endText;
+    @FXML
+    private TextField customerText;
+    @FXML
+    private TextField userText;
+    @FXML
     private Button delete;
     @FXML
-    private Button cancel;
+    private Button cancelButton;
     @FXML
     private Button save;
+    @FXML
+    private Button update;
+
+    public AppointmentDetail() {
+        String url = "../view/AppointmentDetail.fxml";
+        this.viewURL = url;
+    }
+
+    public AppointmentDetail(Appointment appointment) {
+        String url = "../view/AppointmentDetail.fxml";
+        this.viewURL = url;
+        this.appointment = appointment;
+    }
+
+    @Override
+    public String getViewURL() {
+        return viewURL;
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        contact.setItems(DBContact.getContacts());
+
         appointmentTitle.setText(Language.getField("Appointment"));
         idLabel.setText(Language.getField("ID"));
         titleLabel.setText(Language.getField("Title"));
         locationLabel.setText(Language.getField("Location"));
+        descriptionLabel.setText(Language.getField("Description"));
         contactLabel.setText(Language.getField("Contact"));
         typeLabel.setText(Language.getField("Type"));
         startLabel.setText(Language.getField("Start Time"));
@@ -51,7 +106,40 @@ public class AppointmentDetail implements Initializable {
         customerLabel.setText(Language.getField("Customer ID"));
         userLabel.setText(Language.getField("User ID"));
         delete.setText(Language.getField("Delete"));
-        cancel.setText(Language.getField("Cancel"));
+        cancelButton.setText(Language.getField("Cancel"));
         save.setText(Language.getField("Save"));
+
+        delete.setVisible(false);
+        update.setVisible(false);
+
+        if (appointment != null) {
+            idText.setText(String.valueOf(appointment.getId()));
+            titleText.setText(appointment.getTitle());
+            descriptionText.setText(appointment.getDescription());
+            locationText.setText(appointment.getLocation());
+            typeText.setText(appointment.getType());
+            contact.setValue(appointment.getContact());
+            startText.setText(appointment.getStart());
+            endText.setText(appointment.getEnd());
+            customerText.setText(String.valueOf(appointment.getCustomerID()));
+            userText.setText(String.valueOf(appointment.getUserID()));
+
+            delete.setVisible(true);
+            save.setVisible(false);
+            update.setVisible(true);
+        }
+    }
+
+    public void cancel(ActionEvent event) {
+        SceneChange sc = new SceneChange((Stage)((Node)event.getSource()).getScene().getWindow(), "../view/home.fxml");
+        sc.changeScene(Language.getField("Stage Title"), 500, 720);
+    }
+
+    public void updateAppointment(ActionEvent event) {
+        System.out.println("updating appointment...");
+    }
+
+    public void saveAppointment(ActionEvent event) {
+        System.out.println("saving appointment...");
     }
 }
--- src/controller/CustomerDetail.java
+++ src/controller/CustomerDetail.java
@@ -1,15 +1,29 @@
 package controller;
 
+import database.DBCountry;
+import database.DBDivision;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import model.Customer;
 import util.Language;
+import util.SceneChange;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class CustomerDetail implements Initializable {
+public class CustomerDetail implements Initializable, Controller {
+
+    private String viewURL;
+    private Customer customer;
 
     @FXML
     private Label customerTitle;
@@ -28,14 +42,48 @@ public class CustomerDetail implements Initializable {
     @FXML
     private Label phoneLabel;
     @FXML
+    private TextField idText;
+    @FXML
+    private TextField nameText;
+    @FXML
+    private TextField addressText;
+    @FXML
+    private TextField zipcodeText;
+    @FXML
+    private ChoiceBox<String> division;
+    @FXML
+    private ChoiceBox<String> country;
+    @FXML
+    private TextField phoneText;
+    @FXML
     private Button delete;
     @FXML
-    private Button cancel;
+    private Button cancelButton;
     @FXML
     private Button save;
 
+    public CustomerDetail() {
+        String url = "../view/CustomerDetail.fxml";
+        this.viewURL = url;
+    }
+
+    public CustomerDetail(Customer customer) {
+        String url = "../view/CustomerDetail.fxml";
+        this.viewURL = url;
+        this.customer = customer;
+    }
+
+    @Override
+    public String getViewURL() {
+        return viewURL;
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        division.setItems(DBDivision.getDivisions());
+        country.setItems(DBCountry.getCountries());
+
         customerTitle.setText(Language.getField("Customer"));
         idLabel.setText(Language.getField("ID"));
         nameLabel.setText(Language.getField("Name"));
@@ -45,7 +93,35 @@ public class CustomerDetail implements Initializable {
         countryLabel.setText(Language.getField("Country"));
         phoneLabel.setText(Language.getField("Phone"));
         delete.setText(Language.getField("Delete"));
-        cancel.setText(Language.getField("Cancel"));
+        cancelButton.setText(Language.getField("Cancel"));
         save.setText(Language.getField("Save"));
+
+        delete.setVisible(false);
+
+        if (customer != null) {
+            idText.setText(String.valueOf(customer.getId()));
+            nameText.setText(customer.getName());
+            addressText.setText(customer.getAddress());
+            zipcodeText.setText(customer.getZipcode());
+            zipcodeText.setText(customer.getZipcode());
+            country.setValue(customer.getCountry());
+            division.setValue(customer.getDivision());
+            phoneText.setText(customer.getPhone());
+
+            delete.setVisible(true);
+        }
+    }
+
+    public void cancel(ActionEvent event) {
+        SceneChange sc = new SceneChange((Stage)((Node)event.getSource()).getScene().getWindow(), "../view/home.fxml");
+        sc.changeScene(Language.getField("Stage Title"), 500, 720);
+    }
+
+    public void filterDivisions(ActionEvent event) {
+        System.out.println(country.getValue());
+        ObservableList<String> divisions = DBDivision.getDivisions(country.getValue());
+        division.setItems(divisions);
+        // set default value so it's populated
+        division.setValue(divisions.get(0));
     }
 }
