ae6f6511aec62d55e0cccdbb9958a50565ea6b87
nikhuff <nik.huff@me.com>
2021-04-13 22:10:57 -0700

    changes for datetime and min height width

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,21 +8,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import util.Language;
 import util.SceneChange;
+import util.Time;
 
 public class Main extends Application {
 
     @Override
     public void init() {
         Language.init();
+        Time.init();
         DBConnection.startConnection();
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setMinWidth(260);
-        primaryStage.setMinHeight(260);
         SceneChange sc = new SceneChange(primaryStage, "../view/login.fxml");
-        sc.changeScene(Language.getField("Stage Title"), 300, 300);
+        sc.changeScene(Language.getField("Stage Title"), 300, 300, 260, 260);
     }
 
 
