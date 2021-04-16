b596f33c5f74f4fcc9c98fb0b032bea7369132e5
nikhuff <nik.huff@me.com>
2021-04-16 13:40:29 -0700

    add dialog box

--- src/controller/Login.java
+++ src/controller/Login.java
@@ -1,23 +1,20 @@
 package controller;
 
 import database.DBUser;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import model.User;
+import util.DialogBox;
 import util.Language;
 import util.SceneChange;
+import util.Time;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -28,6 +25,8 @@ public class Login implements Initializable {
     private Label username;
     @FXML
     private Label password;
+    @FXML
+    private Label zoneId;
     @FXML
     private Button signIn;
     @FXML
@@ -41,17 +40,19 @@ public class Login implements Initializable {
         username.setText(Language.getField("Username"));
         password.setText(Language.getField("Password"));
         signIn.setText(Language.getField("Sign In"));
+        zoneId.setText(Time.getZoneId().toString());
     }
 
     public void signIn(ActionEvent actionEvent) {
 
-        User user = DBUser.authenticateUser(this.providedUsername.getText(), this.providedPassword.getText());
+        User user = DBUser.authenticateUser(providedUsername.getText(), providedPassword.getText());
         if (user != null) {
             System.out.println("Success! Welcome " + user.getUsername() + "!");
             SceneChange sc = new SceneChange((Stage)((Node)actionEvent.getSource()).getScene().getWindow(), "../view/home.fxml");
-            sc.changeScene(Language.getField("Stage Title"), 500, 500);
+            sc.changeScene(Language.getField("Stage Title"), 500, 500, 500, 500);
         } else {
             System.out.println("Invalid user name or password");
+            DialogBox.invalidCredentials();
         }
     }
 }
