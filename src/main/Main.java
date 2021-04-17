896dbad92505a9f5b3a5a23d394da1f64e904eca
nikhuff <nik.huff@me.com>
2021-04-17 04:11:24 -0700

    remove unused imports

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,9 +2,6 @@ package main;
 
 import database.DBConnection;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import util.Language;
 import util.SceneChange;
